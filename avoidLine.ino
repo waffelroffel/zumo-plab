331b1b12edd2dd3498edcd1d4c456be3d4784455
waffelroffel <waffelroffel@gmail.com>
2017-03-14 17:02:08 +0100

    Update avoidLine.ino

--- avoidLine.ino
+++ avoidLine.ino
@@ -3,11 +3,11 @@
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
 
-#define QTR_THRESHOLD  1800
+#define QTR_THRESHOLD  100
 
 #define TURN_SPEED        400
 #define FORWARD_SPEED     200
-#define TURN_DURATION     140 // ms
+#define TURN_DURATION     160
  
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
@@ -20,6 +20,18 @@ ZumoReflectanceSensorArray sensors;
 void setup(){
   sensors.init();
   button.waitForButton();
+  delay(1000);
+  for(int i = 0; i < 80; i++){
+    if((i > 10 && i <= 30) || (i > 50 && i <= 70)){
+      motors.setSpeeds(-200, 200);
+    }else{
+      motors.setSpeeds(200, -200);
+    }
+    reflectanceSensors.calibrate();
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+  button.waitForButton();
 }
 
 void loop(){
@@ -28,21 +40,20 @@ void loop(){
 
 void avoidLine(){
   sensors.read(sensor_values);
-  if(sensor_values[0] > QTR_THRESHOLD){
+  if(sensor_values[0] < QTR_THRESHOLD){
     // if leftmost sensor detects line, turn to the right
     motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-    delay(70);
-  }else if(sensor_values[5] > QTR_THRESHOLD){
+    delay(50);
+  }else if(sensor_values[5] < QTR_THRESHOLD){
     // if rightmost sensor detects line, turn to the left
     motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-    delay(70);
+    delay(50);
   }else{
     // otherwise, go straight
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
   }
 }
-
