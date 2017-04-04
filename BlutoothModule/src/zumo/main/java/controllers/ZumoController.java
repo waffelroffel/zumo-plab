767229c1254ab1fabe35ca94d867575082564562
strandlie <hstrandlie@gmail.com>
2017-04-04 17:50:14 +0200

    Made much simpler

--- BlutoothModule/src/zumo/main/java/controllers/ZumoController.java
+++ BlutoothModule/src/zumo/main/java/controllers/ZumoController.java
@@ -1,15 +1,14 @@
 package zumo.main.java.controllers;
 
 import zumo.main.java.application.PLabSerial;
-import java.util.Stack;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class ZumoController {
 	
 	private PLabSerial serialConnection = new PLabSerial();
-	private Stack<String> messages = new Stack<>();
 	
 	@FXML
 	private Text lastState;
@@ -23,6 +22,8 @@ public class ZumoController {
 	private Text fourthLastMessageText;
 	@FXML
 	private Text fifthLastMessageText;
+	@FXML
+	private Button exitButton;
 
 	
 	
@@ -36,56 +37,24 @@ public class ZumoController {
 	}
 	
 	public void messageRecieved(String message, int value) {
-		if (message.length() > 0) {
-			messages.add(message);
-		}
-		
-		update();
-	}
-	
-	private void update() {
-		try {
-			lastMessageText.setText(messages.get(0));
-		}
-		catch (ArrayIndexOutOfBoundsException e) {
-			lastMessageText.setText("");
-		}
-
-		
-		try {
-			secondLastMessageText.setText(messages.get(1));
-		}	
-		catch (ArrayIndexOutOfBoundsException e) {
-			secondLastMessageText.setText("");
-		}
-		
-		
-		try {
-			thirdLastMessageText.setText(messages.get(2));
-		}	
-		catch (ArrayIndexOutOfBoundsException e) {
-			thirdLastMessageText.setText("");
-		}
-		
-		
-		try {
-			fourthLastMessageText.setText(messages.get(3));
-		}	
-		catch (ArrayIndexOutOfBoundsException e) {
-			fourthLastMessageText.setText("");
-		}
-		
-		
-		try {
-			fifthLastMessageText.setText(messages.get(4));
-		}	
-		catch (ArrayIndexOutOfBoundsException e) {
-			fifthLastMessageText.setText("");
-		}
-		
+		lastMessageText.setText(message);
 	}
 	
 	
+	@FXML
+	public void disconnect() {
+		System.out.println("Port closed");
+		serialConnection.closePort();
+	}
 	
+	
+	@FXML
+	public void reconnect() {
+		serialConnection.closePort();
+		serialConnection.openPLabPort();
+		
+		System.out.println(serialConnection.getOpenPortName());
+		serialConnection.addListener(this, "messageRecieved");
+	}
 
 }
